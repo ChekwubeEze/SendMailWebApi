743687ae3105019ab5bae7707f514cf8a2973ac0
Ezechekwubetech2022 <ezechekwubeinnocent@gmail.com>
2022-11-21 13:15:25 +0100

    Add project files.

---
+++ SendmailAPI/Controllers/EmailController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SendmailAPI.Interfaces;
+using SendmailAPI.Models;
+
+namespace SendmailAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IMailServices _mailServices;
+
+        public EmailController(IMailServices mailServices)
+        {
+            _mailServices = mailServices;
+        }
+        [HttpPost("Send mail")]
+        public IActionResult Sendmail([FromQuery]EmailReceiver emailReceiver)
+        {
+            _mailServices.SendEmail(emailReceiver);
+            return Ok();
+        }
+    }
+}
---
+++ SendmailAPI/services/EmailServices.cs
@@ -0,0 +1,32 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+using SendmailAPI.Interfaces;
+using SendmailAPI.Models;
+
+namespace SendmailAPI.services
+{
+    public class EmailServices : IMailServices
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailServices(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void SendEmail(EmailReceiver emailReceiver)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("agnes.pouros@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse(emailReceiver.To));
+            email.Subject = emailReceiver.Subject;
+            email.Body = new TextPart(TextFormat.Plain) { Text = emailReceiver.Body};
+            using var smtp = new SmtpClient();
+            smtp.Connect(_configuration.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_configuration.GetSection("EmailUserName").Value, _configuration.GetSection("EmailPassword").Value);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
